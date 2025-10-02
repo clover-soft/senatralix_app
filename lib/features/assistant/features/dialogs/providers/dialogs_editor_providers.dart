9abacb03f6708499e8fdc5c30267cfd664bbf41c
Yakov <yakov@panargin.ru>
2025-10-02 14:14:18 +0300

    feat: dialogs config screen

--- lib/features/assistant/features/dialogs/providers/dialogs_editor_providers.dart
+++ lib/features/assistant/features/dialogs/providers/dialogs_editor_providers.dart
@@ -3,6 +3,8 @@ import 'package:graphview/GraphView.dart';
 import 'package:sentralix_app/features/assistant/features/dialogs/controllers/dialogs_editor_controller.dart';
 import 'package:sentralix_app/features/assistant/features/dialogs/graph/dialogs_graph_builder.dart';
 import 'package:sentralix_app/features/assistant/features/dialogs/graph/graph_style.dart';
+import 'package:sentralix_app/features/assistant/features/dialogs/utils/dialogs_layout_utils.dart';
+import 'package:flutter/foundation.dart';
 
 /// Контроллер редактора
 final dialogsEditorControllerProvider =
@@ -16,5 +18,8 @@ final graphProvider = Provider<Graph>((ref) {
   // Жёстко фиксируем Sugiyama сверху-вниз
   final style = GraphStyle.sugiyamaTopBottom(nodeSeparation: 20, levelSeparation: 80);
   final builder = DialogsGraphBuilder(style: style);
+  // Лог размера графа (ряды/колонки) по текущей конфигурации
+  final stats = computeDialogGridStats(editor.steps);
+  debugPrint('[Dialogs Graph] rows=${stats.rows}, cols=${stats.cols}');
   return builder.build(editor.steps);
 });
